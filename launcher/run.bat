334989a46216cb01703a9ebd592917106dcd4221
yodaaicreator <fortnite.king.konge@gmail.com>
2026-06-28 14:10:26 +0200

    v1.0.7

--- launcher/run.bat
+++ launcher/run.bat
@@ -2,19 +2,10 @@
 cd /d "%~dp0"
 title Orbit
 
-:: Check Node/Electron
-where npx >nul 2>&1
-if errorlevel 1 (
-    echo Node.js not found. Install from nodejs.org
-    pause
-    exit /b 1
-)
-
-:: Install if needed
 if not exist node_modules (
     echo Installing dependencies...
     call npm install
 )
 
-:: Launch
-start "" npx electron .
+npx electron .
+pause
