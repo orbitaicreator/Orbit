efcb952e1fd2602061726c00ff59f9dd81c203af
yodaaicreator <fortnite.king.konge@gmail.com>
2026-07-22 20:04:16 +0200

    v1.0.4

--- launcher/run.bat
+++ launcher/run.bat
@@ -1,5 +1,8 @@
 @echo off
 cd /d "%~dp0"
+:: If this .bat lives in a subfolder (like launcher\), go up to the Orbit root
+if not exist package.json cd ..
+if not exist package.json cd /d "C:\Users\krist\Orbit"
 title Orbit
 
 if not exist node_modules (
